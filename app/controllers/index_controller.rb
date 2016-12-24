e0574cf27ce2bb313e732b71070a27c694a37394
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-12-24 20:27:11 +0100

    use no layout

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,8 @@
 require 'rails/application_controller'
 
 class IndexController < Rails::ApplicationController
+  layout false
+
   def index
     render file: Rails.root.join('app', 'views', 'layouts', 'application.html')
   end
