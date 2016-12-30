6f97ad9e744eabf47a702823a376af115aa8eea0
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-12-30 01:33:50 +0100

    add action for them whole lists

--- app/react/actions/index.js
+++ app/react/actions/index.js
@@ -8,6 +8,20 @@ export const addTodoList = (todoList) => {
   }
 }
 
+export const addTodoLists = (inTodoLists) => {
+  let todoLists = inTodoLists.map(list => {
+    return {
+      id: list.id,
+      name: list.name,
+    }
+  })
+
+  return {
+    type: 'ADD_TODO_LISTS',
+    todoLists
+  }
+}
+
 export const fetchTodoLists = (todoLists) => {
   return {
     type: 'FETCH_TODO_LISTS',
